9db51341db505dd6bcee6af823f3b5050c59adb2
Dillon Townsend <townsenddillon.dt@gmail.com>
2021-09-09 22:08:53 -0500

    added gitignore file

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,8 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-with open(os.path.join(BASE_DIR, 'secret_key.txt')) as f:
-    SECRET_KEY = f.read().strip()
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
